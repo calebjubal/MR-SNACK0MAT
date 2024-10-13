de5c3a26a30e898555e2749a1745c08441530723
BB9Kairo <85286527+calebjubal@users.noreply.github.com>
2024-10-13 10:05:05 +0000

    Adding Tailwind CSS with a CDN link

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Your personal snackbot to assist you in making the perfect friday snack combo</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Nosifer&family=Roboto&display=swap" rel="stylesheet">
